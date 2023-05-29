72f88ecb15b59da9de1aa354d8b295994040c5c2
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-05-29 11:56:25 -0300

    subservice TU

---
+++ test/shared/domain/entities/subservice_test.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+import 'package:clean_flutter_template/generated/l10n.dart';
+import 'package:clean_flutter_template/shared/domain/entities/subservice.dart';
+import 'package:clean_flutter_template/shared/domain/enums/subservice_type_enum.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(() async {
+    await S.load(const Locale.fromSubtags(languageCode: 'en'));
+  });
+
+  test('[TEST] - adress', () {
+    expect(
+      () => Subservice(
+        subserviceType: SubserviceTypeEnum.LANDSCAPE_AREAS,
+        orientation: 'orientation',
+        unit: 'unit',
+        quantity: 1,
+      ),
+      isNotNull,
+    );
+  });
+}
